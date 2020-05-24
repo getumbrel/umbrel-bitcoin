afb6e22a5c31d3a02382a8868b059f3bb78a86ae
nolim1t <hello@nolim1t.co>
2020-05-24 21:39:22 +0700

    Add wait for script

--- wait-for-node-manager.sh
+++ wait-for-node-manager.sh
@@ -1,20 +1,23 @@
-#!/usr/bin/env node
+#!/bin/bash
 
-const pingurl = process.env.PINGURL || "http://localhost:3006/ping";
+set -e
 
-const axios = require('axios').default;
-axios.get(pingurl).then(
-    (resp) => {
-        console.log(resp.data);
-        process.exit(0);
-    }).catch(
-        (error) => {
-            if (error.response != undefined || error.response != null ) {
-                console.log(error.response.status);
-            } else {
-                console.log("Error connecting to: " + pingurl);
-            }
-            process.exit(1)
-        }
-    );
 
+host="$1"
+shift
+cmd="$@"
+
+found=1
+
+until [ $found = 2 ]; do
+  if node -e "const axios = require('axios').default; axios.get('http://$host:3006/ping').then((resp) => {console.log(resp.data); process.exit(0); }).catch((error) => {process.exit(1) } );" ; then
+    echo "Executed"
+    found=2
+  else
+    echo "Can't connect, keep trying"
+  fi
+  sleep 2
+done
+
+>&2 echo "Pre-condition found, Running service"
+exec $cmd
