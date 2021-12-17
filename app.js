99aa5c9c58cfd095010f195e1a0e0152003957e2
Kevin Mulcrone <kaybesee@gmail.com>
2021-12-16 19:51:27 -0700

    remove cors options

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-require('module-alias/register');
-require('module-alias').addPath('.');
-require('dotenv').config();
+require("module-alias/register");
+require("module-alias").addPath(".");
+require("dotenv").config();
 
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Keep requestCorrelationId middleware as the first middleware. Otherwise we risk losing logs.
-const requestCorrelationMiddleware = require('middlewares/requestCorrelationId.js'); // eslint-disable-line id-length
-const camelCaseReqMiddleware = require('middlewares/camelCaseRequest.js').camelCaseRequest;
-const corsOptions = require('middlewares/cors.js').corsOptions;
-const errorHandleMiddleware = require('middlewares/errorHandling.js');
+const requestCorrelationMiddleware = require("middlewares/requestCorrelationId.js"); // eslint-disable-line id-length
+const camelCaseReqMiddleware =
+  require("middlewares/camelCaseRequest.js").camelCaseRequest;
+const errorHandleMiddleware = require("middlewares/errorHandling.js");
 
-const logger = require('utils/logger.js');
+const logger = require("utils/logger.js");
 
-const bitcoind = require('routes/v1/bitcoind/info.js');
-const ping = require('routes/ping.js');
+const bitcoind = require("routes/v1/bitcoind/info.js");
+const ping = require("routes/ping.js");
 const app = express();
 
 // Handles CORS
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(requestCorrelationMiddleware);
 app.use(camelCaseReqMiddleware);
 app.use(morgan(logger.morganConfiguration));
 
-app.use('/v1/bitcoind/info', bitcoind);
-app.use('/ping', ping);
+app.use("/v1/bitcoind/info", bitcoind);
+app.use("/ping", ping);
 
 app.use(errorHandleMiddleware);
 app.use((req, res) => {
