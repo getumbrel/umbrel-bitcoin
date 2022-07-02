fdba11e1c3169486c5636089f499174cc6e3e6b2
Fernando Falci <fernando.falci@adevinta.com>
2022-07-02 13:51:39 +0200

    chore(build): updated GitHub Actions

--- utils/const.js
+++ utils/const.js
@@ -2,10 +2,8 @@
 module.exports = {
   HSD_PORT: process.env.HSD_PORT || 12037,
   HSD_API_KEY: process.env.HSD_API_KEY || 'moneyprintergobrrr',
-  DEVICE_HOSTNAME: process.env.DEVICE_HOSTNAME || 'umbrel.local',
+  DEVICE_DOMAIN_NAME: process.env.DEVICE_DOMAIN_NAME,
 
   REQUEST_CORRELATION_NAMESPACE_KEY: 'umbrel-manager-request',
   REQUEST_CORRELATION_ID_KEY: 'reqId',
-  BITCOIN_RPC_USER: process.env.BITCOIN_RPC_USER || 'umbrel',
-  DEVICE_DOMAIN_NAME: process.env.DEVICE_DOMAIN_NAME,
 };
