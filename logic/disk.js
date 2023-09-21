4030c212a4a97778eeb47a3c7a8434b7ec64261a
Kroese <kroese@users.noreply.github.com>
2023-09-21 12:37:14 -0700

    whitelist internal umbrel ip range
    Co-authored-by: Nathan Fretz <nmfretz@gmail.com>

--- logic/disk.js
+++ logic/disk.js
@@ -166,6 +166,10 @@ function settingsToMultilineConfString(settings) {
   umbrelBitcoinConfig.push(`listenonion=${listen}`);
   umbrelBitcoinConfig.push(`i2pacceptincoming=${listen}`);
 
+  // whitelist
+  umbrelBitcoinConfig.push(`# Whitelist peers connecting from local Umbrel IP range. Whitelisted peers cannot be DoS banned and their transactions are always relayed, even if they are already in the mempool.`);
+  umbrelBitcoinConfig.push(`whitelist=10.21.0.0/16`);
+
   // [RPC]
   umbrelBitcoinConfig.push("");
   umbrelBitcoinConfig.push("# [rpc]"); 
