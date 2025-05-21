4c9f805473014e9752682f92ec5a1105e933df52
nmfretz <nmfretz@gmail.com>
2025-05-21 22:12:05 +1000

    Add settings card placeholder

--- apps/ui/src/pages/settings/index.tsx
+++ apps/ui/src/pages/settings/index.tsx
@@ -1,3 +1,13 @@
+import {Card, CardHeader, CardTitle} from '@/components/ui/card'
+import {GradientBorderFromTop} from '@/components/shared/GradientBorders'
+
 export default function SettingsPage() {
-	return <p className='text-xl text-white'>Settings page placeholder</p>
+	return (
+		<Card className='bg-card-gradient backdrop-blur-2xl border-none rounded-3xl py-4 h-[1000px]'>
+			<GradientBorderFromTop />
+			<CardHeader>
+				<CardTitle className='font-outfit text-white text-[20px] font-[400] pt-2'>Settings</CardTitle>
+			</CardHeader>
+		</Card>
+	)
 }
