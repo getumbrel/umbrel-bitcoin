1afa664431a3b083019806e0d2700921e6e22054
nmfretz <nmfretz@gmail.com>
2025-06-18 10:40:28 +1000

    Bring in rpcClient host IP as env vat

--- apps/backend/src/modules/bitcoind/rpc-client.ts
+++ apps/backend/src/modules/bitcoind/rpc-client.ts
@@ -1,7 +1,7 @@
 import Client from 'bitcoin-core'
 
 export const rpcClient = new Client({
-	host: `http://127.0.0.1:${process.env['RPC_PORT'] || '8332'}`,
+	host: `http://${process.env['BITCOIND_IP'] || '127.0.0.1'}:${process.env['RPC_PORT'] || '8332'}`,
 	username: process.env['RPC_USER'] || 'umbrel',
 	password: process.env['RPC_PASS'] || 'moneyprintergobrrr',
 })
