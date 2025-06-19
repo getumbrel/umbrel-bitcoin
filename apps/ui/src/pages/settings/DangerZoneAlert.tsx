c8336078ef950183f2f69c947ef28ae5b5743226
nmfretz <nmfretz@gmail.com>
2025-06-19 23:50:14 +1000

    Align Advanced settings alert styles with connect modal's

--- apps/ui/src/pages/settings/DangerZoneAlert.tsx
+++ apps/ui/src/pages/settings/DangerZoneAlert.tsx
@@ -1,12 +1,12 @@
 import {Alert, AlertDescription, AlertTitle} from '@/components/ui/alert'
-import {AlertCircle} from 'lucide-react'
+import {TriangleAlert} from 'lucide-react'
 
 export default function DangerZoneAlert() {
 	return (
-		<Alert className='border-yellow-700/50 bg-yellow-600/10 text-yellow-100'>
-			<AlertCircle className='h-4 w-4 text-yellow-500' />
-			<AlertTitle className='text-yellow-100'>Danger Zone</AlertTitle>
-			<AlertDescription className='text-yellow-200/80'>
+		<Alert className='bg-[#EDCE0017] text-[#EDCE00] border-none'>
+			<TriangleAlert className='h-4 w-4' />
+			<AlertTitle className='text-[#EDCE00]'>Danger Zone</AlertTitle>
+			<AlertDescription className='text-[#EDCE00]'>
 				Any changes you make here are not validated. Please ensure that you know what you are doing, and that you
 				understand how changes may impact both your Bitcoin node and any apps or wallets that rely on it.
 			</AlertDescription>
